bf277536a4c66c20f9e50a95acf1cb298dd5358a
Daniel Miller <millerdev@gmail.com>
2014-12-30 20:37:26 -0500

    Prepare for 1.7.0 release

--- editxt/__init__.py
+++ editxt/__init__.py
@@ -21,6 +21,6 @@
 # TODO make __version__ more specific (include date and git revision)
 import logging
 
-__version__ = "1.6.0"
+__version__ = "1.7.0"
 
 log = logging.getLogger(__name__) # for logging unhandled errors
